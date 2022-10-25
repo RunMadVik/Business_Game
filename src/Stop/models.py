9ac331c2d638b4cf8c4f0ea50d722f53ef108a18
Vikhyat Gupta <vikhyat@suraasa.com>
2022-10-25 19:23:59 +0530

    fix: updated models to accomodate larger rent values

--- src/Stop/models.py
+++ src/Stop/models.py
@@ -16,7 +16,7 @@ class Property(models.Model):
     property_type = models.IntegerField(
         choices=PropertyType.choices, default=PropertyType.PLACE
     )
-    rent = models.DecimalField(max_digits=5, decimal_places=2)
+    rent = models.DecimalField(max_digits=10, decimal_places=2)
     cost_of_acquisition = models.DecimalField(max_digits=10, decimal_places=2)
     bank_mortgage_value = models.DecimalField(max_digits=10, decimal_places=2)
     mortgaged = models.BooleanField(default=False)
@@ -38,14 +38,15 @@ class Place(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     property = models.OneToOneField(Property, on_delete=models.CASCADE)
     property_color = models.CharField(max_length=10, choices=COLOR_CHOICES)
-    rent_with_one_house = models.DecimalField(max_digits=5, decimal_places=2)
-    rent_with_two_house = models.DecimalField(max_digits=5, decimal_places=2)
-    rent_with_three_house = models.DecimalField(max_digits=5, decimal_places=2)
-    rent_with_hotel = models.DecimalField(max_digits=5, decimal_places=2)
-    cost_of_house = models.DecimalField(max_digits=5, decimal_places=2)
-    cost_of_hotel = models.DecimalField(max_digits=5, decimal_places=2)
+    rent_with_one_house = models.DecimalField(max_digits=10, decimal_places=2)
+    rent_with_two_house = models.DecimalField(max_digits=10, decimal_places=2)
+    rent_with_three_house = models.DecimalField(max_digits=10, decimal_places=2)
+    rent_with_hotel = models.DecimalField(max_digits=10, decimal_places=2)
+    cost_of_house = models.DecimalField(max_digits=10, decimal_places=2)
+    cost_of_hotel = models.DecimalField(max_digits=10, decimal_places=2)
 
 
 class SpecialStop(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     name = models.CharField(max_length=30)
+    mapped_to = GenericRelation(Mapping)
