a087af60b5e932fcfd681f9254e9f168dd47a15d
Vikhyat Gupta <vikhyat@suraasa.com>
2022-10-28 15:24:09 +0530

    fix: changed get_player service to get-or_create_player

--- src/Player/services/create.py
+++ src/Player/services/create.py
@@ -2,17 +2,28 @@ from typing import Optional, Tuple, Union
 
 from django.core.exceptions import ValidationError
 
+from Lobby.models import Lobby
 from Player.models import Player
 from Users.models import User
 
 
-def create_player(
-    user: User, money: int, name=Optional[str]
+def get_or_create_player(
+    user: User, lobby: Lobby, money: Optional[int] = None, name: Optional[str] = None
 ) -> Tuple[bool, Union[str, Player]]:
-    player = Player(user=user, money=money, name=name or user.get_short_name())
     try:
-        player.save()
-    except ValidationError as error:
-        return False, str(error)
+        player = Player.objects.get(user=user, lobby=lobby)
+    except Player.DoesNotExist:
+        player = Player(
+            user=user,
+            lobby=lobby,
+            money=money or lobby.starting_money,
+            name=name or user.get_short_name(),
+        )
+        try:
+            player.save()
+        except ValidationError as error:
+            return False, str(error)
+
+        return True, player
 
     return True, player
