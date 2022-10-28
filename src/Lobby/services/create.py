b992c2ac72c336a6ea566377fb98301b1cc7b7d2
Vikhyat Gupta <vikhyat@suraasa.com>
2022-10-28 15:42:07 +0530

    fix: fixed code breaks in get or create lobby service

--- src/Lobby/services/create.py
+++ src/Lobby/services/create.py
@@ -15,27 +15,26 @@ def get_or_create_lobby(
     password: Optional[str] = None,
     starting_money: Optional[int] = None,
 ) -> Tuple[bool, Union[Lobby, str]]:
-    if name:
-        try:
+    try:
+        if name:
             lobby = Lobby.objects.get(name=name, created_by=user)
-        except Lobby.DoesNotExist:
-            lobby = Lobby(
-                name=name or generate_random_string(length=10),
-                password=password or generate_random_string(length=10),
-                starting_money=starting_money or 30000,
-                created_by=user,
-            )
-            try:
-                lobby.save()
-            except ValidationError as error:
-                return False, str(error)
-
-            success, player = get_or_create_player(
-                user=user, lobby=lobby, money=starting_money or 30000
-            )
-            if not success:
-                return success, player
-
             return True, lobby
+    except Lobby.DoesNotExist:
+        lobby = Lobby(
+            name=name or generate_random_string(length=10),
+            password=password or generate_random_string(length=10),
+            starting_money=starting_money or 30000,
+            created_by=user,
+        )
+        try:
+            lobby.save()
+        except ValidationError as error:
+            return False, str(error)
 
-    return True, lobby
+        success, player = get_or_create_player(
+            user=user, lobby=lobby, money=starting_money or 30000
+        )
+        if not success:
+            return success, player
+
+        return True, lobby
