b4c7b1434ddaf7cd4178b2fff210b66ff89758bd
Vikhyat Gupta <vikhyat@suraasa.com>
2022-10-28 15:22:10 +0530

    fix: added starting money to lobby model and removed LobbyPlayer model

--- src/Lobby/models.py
+++ src/Lobby/models.py
@@ -1,9 +1,7 @@
 from uuid import uuid4
 
-from django.core.exceptions import ValidationError
 from django.db import models
 
-from Player.models import Player
 from Users.models import User
 
 
@@ -11,6 +9,7 @@ class Lobby(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     name = models.CharField(max_length=50)
     password = models.CharField(max_length=64)
+    starting_money = models.IntegerField()
     created_by = models.ForeignKey(User, on_delete=models.DO_NOTHING)
 
     def __str__(self) -> str:
@@ -18,19 +17,3 @@ class Lobby(models.Model):
 
     class Meta:
         verbose_name_plural = "Lobbies"
-
-
-class LobbyPlayer(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
-    lobby = models.ForeignKey(Lobby, on_delete=models.CASCADE)
-    player = models.ForeignKey(Player, on_delete=models.CASCADE)
-
-    def __str__(self) -> str:
-        return f"{self.player} in lobby {self.lobby}"
-
-    def clean(self, *args, **kwargs):
-        if LobbyPlayer.objects.filter(lobby_id=self.lobby.id).count() == 4:
-            raise ValidationError("Lobby can't have more than 4 players.")
-
-    class Meta:
-        unique_together = ("lobby", "player")
