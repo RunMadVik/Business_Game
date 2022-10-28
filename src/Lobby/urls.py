e286d4141a7af8f71f3448f4e7db493b40536296
Vikhyat Gupta <vikhyat@suraasa.com>
2022-10-28 17:14:06 +0530

    feat: added URL for lobby joining API

--- src/Lobby/urls.py
+++ src/Lobby/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from Lobby.views import CreateLobby, GetLobby
+from Lobby.views import CreateLobby, CreateLobbyPlayer, GetLobby
 
 urlpatterns = [
     path("create/", CreateLobby.as_view(), name="create-lobby"),
     path("<uuid:pk>/", GetLobby.as_view(), name="get-lobby"),
+    path("join/", CreateLobbyPlayer.as_view(), name="join-lobby"),
 ]
